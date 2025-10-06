cabb46c30468b34fe2a3ea370b89bc828a48d53f
Erik Kronquist <erik.kronquist@gmail.com>
2025-10-06 10:53:11 -0600

    End lecture 244

---
+++ lib/screens/places_detail.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_favorite_places_app/models/place.dart';
+
+class PlacesDetailScreen extends StatelessWidget {
+  const PlacesDetailScreen({super.key, required this.place});
+
+  final Place place;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(place.title),
+      ),
+      body: Center(
+        child: Text(
+          place.title,
+          style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+            color: Theme.of(context).colorScheme.onSurface,
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/widgets/places_list.dart
+++ lib/widgets/places_list.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_favorite_places_app/models/place.dart';
+import 'package:flutter_favorite_places_app/screens/places_detail.dart';
 
 class PlacesList extends StatelessWidget {
   const PlacesList({super.key, required this.places});
@@ -26,6 +27,13 @@ class PlacesList extends StatelessWidget {
             color: Theme.of(context).colorScheme.onSurface,
           ),
         ),
+        onTap: () {
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (ctx) => PlacesDetailScreen(place: places[index]),
+            ),
+          );
+        },
       ),
       itemCount: places.length,
     );

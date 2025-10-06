aadb80b9bb063b96af11292f3d0f31f77ca21aaf
Erik Kronquist <erik.kronquist@gmail.com>
2025-10-06 09:56:04 -0600

    End lecture 239

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 final colorScheme = ColorScheme.fromSeed(
@@ -26,7 +26,7 @@ final theme = ThemeData().copyWith(
 
 void main() {
   runApp(
-    const MyApp(),
+    const ProviderScope(child: const MyApp()),
   );
 }
 

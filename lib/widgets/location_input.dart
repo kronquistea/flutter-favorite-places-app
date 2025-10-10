f9e386e26b2a1f3a066482b80430fdc851b38e0f
Erik Kronquist <erik.kronquist@gmail.com>
2025-10-09 20:35:22 -0600

    End lecture 254

--- lib/widgets/location_input.dart
+++ lib/widgets/location_input.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:location/location.dart';
+import 'package:http/http.dart' as http;
 
 class LocationInput extends StatefulWidget {
   const LocationInput({super.key});
@@ -45,13 +48,21 @@ class _LocationInputState extends State<LocationInput> {
 
     locationData = await location.getLocation();
 
+    final lat = locationData.latitude;
+    final lng = locationData.longitude;
+    const apiKey = 'AIzaSyAWjDtXdIpu7sm8xe8L-9SJxPFqojkFpL4';
+
+    final url = Uri.parse(
+      'https://maps.googleapis.com/maps/api/geocode/json?latlng=$lat,$lng&key=$apiKey',
+    );
+    final response = await http.get(url);
+    final responseData = jsonDecode(response.body);
+    final address = responseData['results'][0]['formatted_address'];
+
     // Remove loading spinner
     setState(() {
       _isGettingLocation = false;
     });
-
-    print(locationData.latitude);
-    print(locationData.longitude);
   }
 
   @override

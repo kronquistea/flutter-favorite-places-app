a4cb903a48178a845dd37c61b97a85352327d5ee
Erik Kronquist <erik.kronquist@gmail.com>
2025-10-11 11:20:51 -0600

    End lecture 265

--- lib/providers/user_places.dart
+++ lib/providers/user_places.dart
@@ -1,5 +1,7 @@
 import 'dart:io';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:path_provider/path_provider.dart' as syspaths;
+import 'package:path/path.dart' as path;
 import 'package:flutter_favorite_places_app/models/place.dart';
 
 // Riverpod notifier to be used for adding favorite places
@@ -7,10 +9,17 @@ class UserPlacesNotifier extends Notifier<List<Place>> {
   @override
   List<Place> build() => const [];
 
-  void addPlace(String title, File image, PlaceLocation location) {
+  void addPlace(String title, File image, PlaceLocation location) async {
+    // Get OS specific data storage path (using path_provider)
+    final appDir = await syspaths.getApplicationDocumentsDirectory();
+    // Get only the name of the file (ex. foo.dart)
+    final fileName = path.basename(image.path);
+    // Path MUST include target path AND file name
+    final copiedImage = await image.copy('${appDir.path}/$fileName');
+
     final newPlace = Place(
       title: title,
-      image: image,
+      image: copiedImage,
       location: location,
     );
     state = [newPlace, ...state];

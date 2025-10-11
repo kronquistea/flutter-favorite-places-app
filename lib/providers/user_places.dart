b47b30edd76d4da6deb680e502be3d0ade74bfba
Erik Kronquist <erik.kronquist@gmail.com>
2025-10-11 11:42:32 -0600

    End lecture 267

--- lib/providers/user_places.dart
+++ lib/providers/user_places.dart
@@ -7,10 +7,49 @@ import 'package:sqflite/sqlite_api.dart';
 
 import 'package:flutter_favorite_places_app/models/place.dart';
 
+// This should be changed to a dart style getter I think?
+Future<Database> _getDatabase() async {
+  final dbPath = await sql.getDatabasesPath();
+  // Second parameter for join is the database name (that I chose)
+  final db = await sql.openDatabase(
+    path.join(dbPath, 'places.db'),
+    onCreate: (db, verison) {
+      return db.execute(
+        'CREATE TABLE user_places (id TEXT PRIMARY KEY, title TEXT, image TEXT, lat REAL, lng REAL, address TEXT)',
+      );
+    },
+    // Should be increased if the query ever changes, so that a new DB file
+    // is created if the DB structure changes
+    version: 1,
+  );
+  return db;
+}
+
 // Riverpod notifier to be used for adding favorite places
 class UserPlacesNotifier extends Notifier<List<Place>> {
   @override
   List<Place> build() => const [];
+
+  void loadPlaces() async {
+    final db = await _getDatabase();
+    final data = await db.query('user_places');
+    final places = data
+        .map(
+          (row) => Place(
+            id: row['id'] as String,
+            title: row['title'] as String,
+            image: File(row['image'] as String),
+            location: PlaceLocation(
+              latitude: row['lat'] as double,
+              longitude: row['lng'] as double,
+              address: row['address'] as String,
+            ),
+          ),
+        )
+        .toList();
+
+    state = places;
+  }
 
   void addPlace(String title, File image, PlaceLocation location) async {
     // Get OS specific data storage path (using path_provider)
@@ -26,20 +65,7 @@ class UserPlacesNotifier extends Notifier<List<Place>> {
       location: location,
     );
 
-    final dbPath = await sql.getDatabasesPath();
-    // Second parameter for join is the database name (that I chose)
-    final db = await sql.openDatabase(
-      path.join(dbPath, 'places.db'),
-      onCreate: (db, verison) {
-        return db.execute(
-          'CREATE TABLE user_places (id TEXT PRIMARY KEY, title TEXT, image TEXT, lat REAL, lng REAL, address TEXT)',
-        );
-      },
-      // Should be increased if the query ever changes, so that a new DB file
-      // is created if the DB structure changes
-      version: 1,
-    );
-
+    final db = await _getDatabase();
     final values = {
       'id': newPlace.id,
       'title': newPlace.title,

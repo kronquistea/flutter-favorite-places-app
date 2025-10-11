31842e3ff0cef7cacfc7a5616e09a92dbaeeaceb
Erik Kronquist <erik.kronquist@gmail.com>
2025-10-11 11:32:37 -0600

    End lecture 266

--- lib/providers/user_places.dart
+++ lib/providers/user_places.dart
@@ -2,6 +2,9 @@ import 'dart:io';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:path_provider/path_provider.dart' as syspaths;
 import 'package:path/path.dart' as path;
+import 'package:sqflite/sqflite.dart' as sql;
+import 'package:sqflite/sqlite_api.dart';
+
 import 'package:flutter_favorite_places_app/models/place.dart';
 
 // Riverpod notifier to be used for adding favorite places
@@ -22,6 +25,31 @@ class UserPlacesNotifier extends Notifier<List<Place>> {
       image: copiedImage,
       location: location,
     );
+
+    final dbPath = await sql.getDatabasesPath();
+    // Second parameter for join is the database name (that I chose)
+    final db = await sql.openDatabase(
+      path.join(dbPath, 'places.db'),
+      onCreate: (db, verison) {
+        return db.execute(
+          'CREATE TABLE user_places (id TEXT PRIMARY KEY, title TEXT, image TEXT, lat REAL, lng REAL, address TEXT)',
+        );
+      },
+      // Should be increased if the query ever changes, so that a new DB file
+      // is created if the DB structure changes
+      version: 1,
+    );
+
+    final values = {
+      'id': newPlace.id,
+      'title': newPlace.title,
+      'image': newPlace.image.path,
+      'lat': newPlace.location.latitude,
+      'lng': newPlace.location.longitude,
+      'address': newPlace.location.address,
+    };
+    db.insert('user_places', values);
+
     state = [newPlace, ...state];
   }
 }
